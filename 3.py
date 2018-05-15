4e431f61f6b36591f1eb3c9e683aeebf918d22f9
Bicky23 <bijitdeka23@gmail.com>
2018-05-15 17:34:02 +0530

    completed third

--- 3.py
+++ 3.py
@@ -1,11 +1,9 @@
-seq = [1,2]
-first_num = 1
-last_num = 2
-res = 0
-while last_num <= 4000000:
-    new_num = first_num + last_num
-    first_num, last_num = last_num, new_num
-    if first_num % 2 == 0:
-        res += first_num
+num = 600851475143 
+i = 2
 
-print(res)
+while i**2 < num:
+    while num%i == 0:
+        num = num/i
+    i += 1
+
+print(num)
